72ad4fa30002b0fcdcc4a9d01975cfd119e7b826
Vladimir <andiwillfly@gmail.com>
2020-04-15 13:28:55 +0300

    make it better

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
+const express = require('express');
 const functions = require('firebase-functions');
+// const mime = require('mime');
+const puppeteer = require('puppeteer');
 
+// const app = express();
+// app.use(function cors(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     // res.header('Content-Type', 'application/json;charset=utf-8');
+//     // res.header('Cache-Control', 'private, max-age=300');
+//     next();
+// });
+//
+// // const beforeMB = process.memoryUsage().heapUsed / 1e6;
+// // puppeteer.launch().then(browser => {
+// //   app.locals.browser = browser;
+// //   const afterMB = process.memoryUsage().heapUsed / 1e6;
+// //   console.log('used', beforeMB - afterMB + 'MB');
+// // })
+//
+// app.get('/test', (req, res) => {
+//     res.status(200).send('test');
+// });
+//
+// // Init code that gets run before all request handlers.
+// app.all('*', async (req, res, next) => {
+//     res.locals.browser = await puppeteer.launch({args: ['--no-sandbox']});
+//     next(); // pass control on to router.
+// });
+
+
+// exports.test = functions.https.onRequest(app);
+
+// const translate = require('./translate.function');
+
+const cors = require('cors')({origin: true});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        response.send({h: "Hello from Firebase!"});
+    })
+});
---
+++ functions/yandex.parser.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer-extra');
+
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')());
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+
+
+let browser = null;
+const group = 'Asking Alexandria';
+const album = 'Down To Hell';
+
+
+async function parsePage(browser) {
+    try {
+        const page = await browser.newPage();
+        await page.goto(`https://music.yandex.ua`, {
+            waitUntil: 'networkidle2'
+        });
+
+        await page.waitFor(3000);
+
+        await page.evaluate((_group)=> {
+            const email = document.querySelector('input.deco-input_suggest');
+            email.value = _group;
+        }, group);
+        await page.click('button.suggest-button');
+        await page.waitFor(3000);
+
+        const artistLink = await page.evaluate((_group)=> {
+            const $artistLink = [ ...document.querySelectorAll('.serp-snippet__artists > .artist .artist__name a') ]
+                .find($artist => $artist.innerText === 'Asking Alexandria');
+
+            return $artistLink ? $artistLink.getAttribute('href') : null;
+        }, group);
+
+        if(!artistLink) return { error: `No such group: ${group}` };
+
+        console.log('---->', `https://music.yandex.ua${artistLink}`);
+
+        await page.goto(`https://music.yandex.ua${artistLink}/albums`, {
+            waitUntil: 'networkidle2'
+        });
+        await page.waitFor(3000);
+
+
+        const albumLink = await page.evaluate((_album)=> {
+            const $album = [...document.querySelector('.page-artist__albums').querySelectorAll('.album')]
+                .find($artist => $artist.querySelector('.album__title.typo-main').innerText === 'Down To Hell');
+
+            return $album ? $album.querySelector('a').getAttribute('href') : null;
+        }, album)
+
+        if(!albumLink) return { error: `No such album: ${album}` };
+
+        console.log('✨YANDEX ENTER page', `https://music.yandex.ua${albumLink}`);
+        return { link: `https://music.yandex.ua${albumLink}` };
+
+    } catch(e) {
+        return { error: e };
+    }
+}
+
+
+async function start() {
+    console.log('YANDEX PARSER:START');
+
+    //  const browser = await puppeteer.launch({
+    //         args: [`--proxy-server=${newProxyUrl}`],
+    //     });
+    browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
+
+    const response = await parsePage(browser);
+    browser.close();
+
+    console.log('YANDEX PARSER:END', response);
+    return response;
+}
+
+module.exports = start;
