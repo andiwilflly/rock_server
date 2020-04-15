b5374a5f90ae8cbb2fc447889a4721877750e848
Vladimir <andiwillfly@gmail.com>
2020-04-15 13:49:34 +0300

    her

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.get('/find/:group/:album', function (req, res) {
+    // request.params.group;
+    // request.params.album;
+    res.send('find!', req.params.group, req.params.album);
+});
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
