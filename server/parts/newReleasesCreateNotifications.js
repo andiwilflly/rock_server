34065afe8330b4a18948c8bd26cadc4eec2a1859
Vladimir <andiwillfly@gmail.com>
2020-05-27 20:13:05 +0300

    her

--- server/parts/newReleasesCreateNotifications.js
+++ server/parts/newReleasesCreateNotifications.js
@@ -2,16 +2,23 @@ const cron = require('node-cron');
 const admin = require("firebase-admin");
 const request = require('request');
 const firebase = require('firebase');
+// Utils
+const setupBrowser = require('../utils/setupBrowser.utils');
 // Parts
 const searchAlbumYouTube = require('./youtube/searchAlbum.youtube.api');
 const lastFmParser = require('../../@parsers/last.fm.parser');
 const yandexParser = require('../../@parsers/yandex.parser');
+const soundcloudParser = require('../../@parsers/soundcloud.parser');
+const googleParser = require('../../@parsers/google.parser');
+const appleParser = require('../../@parsers/apple.parser');
 
 
 module.exports = async function(NEW_RELEASES = {}) {
     const DB = firebase.firestore();
 
     global.LOG.info('SENDER | RECEIVE: releases ', Object.keys(NEW_RELEASES));
+
+    const browser = await setupBrowser();
 
     for(const newRelease of Object.values(NEW_RELEASES)) {
 
@@ -21,7 +28,12 @@ module.exports = async function(NEW_RELEASES = {}) {
 
         let youtube = await searchAlbumYouTube(newRelease.artist, newRelease.name);
         let lastfm = await lastFmParser(newRelease.artist, newRelease.name);
-        let yandex = await yandexParser(newRelease.artist, newRelease.name);
+       // let yandex = await yandexParser(browser, newRelease.artist, newRelease.name);
+        //let soundcloud = await soundcloudParser(browser, newRelease.artist, newRelease.name);
+        //let google = await googleParser(browser, newRelease.artist, newRelease.name);
+        //let apple = await appleParser(browser, newRelease.artist, newRelease.name);
+
+        browser.close();
 
         // let lastfm = await fetch(`${global.BASE_URL}/find/${encodeURIComponent(newRelease.artist)}/${encodeURIComponent(newRelease.name)}?q=lastfm`);
         // lastfm = await lastfm.json();
@@ -50,7 +62,11 @@ module.exports = async function(NEW_RELEASES = {}) {
                     // apple: apple[0].link || '',
                     youtube,
                     lastfm,
-                    yandex,
+                    //yandex,
+                   // soundcloud,
+                   // google,
+                   // apple
+
                     // lastfm: lastfm[0].link || '',
                     // google: google[0].link || '',
                     // soundcloud: soundcloud[0].link  || '',
---
+++ server/utils/setupBrowser.utils.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')());
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+
+
+module.exports = async function setupBrowser() {
+    const browser = await puppeteer.launch({
+        headless: true,
+        ignoreDefaultArgs: ["--mute-audio", "--hide-scrollbars"],
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu',
+            '--window-size=1920x1080',
+        ]
+    });
+    global.LOG.info('BROWSER | SETUP: success');
+
+    return browser;
+}
