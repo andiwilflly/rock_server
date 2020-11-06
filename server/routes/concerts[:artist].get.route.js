4197df51a1544c90ba1ddf8f0adadd0d847d6619
Vladimir <andiwillfly@gmail.com>
2020-11-06 13:59:13 +0200

    Dss

--- server/routes/concerts[:artist].get.route.js
+++ server/routes/concerts[:artist].get.route.js
@@ -1,3 +1,6 @@
+const NodeCache = require( "node-cache" );
+const cache = new NodeCache( { stdTTL: 100, checkperiod: 120 } );
+
 const bandsintownParser = require('../../@parsers/bandsintown.parser');
 // Utils
 const setupBrowser = require('../utils/setupBrowser.utils');
@@ -6,6 +9,12 @@ const setupBrowser = require('../utils/setupBrowser.utils');
 let browser = null;
 
 module.exports = async function (req, res) {
+    const artist = req.params.artist.trim();
+    const key = `concerts/${artist}`;
+
+    const cachedResults = cache.get(key);
+
+    if(cachedResults) return res.send(cachedResults);
 
     if(browser) {
         res.status(500).send('Another parser in progress...');
@@ -13,8 +22,6 @@ module.exports = async function (req, res) {
     }
 
     browser = await setupBrowser();
-
-    const artist = req.params.artist.trim();
 
     console.time(`👮 FIND CONCERTS | ${artist}`);
 
@@ -25,5 +32,6 @@ module.exports = async function (req, res) {
 
     console.timeEnd();
 
+    cache.set(key, results, 86400000); // 1 day
     res.send(results);
 };
