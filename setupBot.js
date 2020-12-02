9565bed5ac0d6bfbe9a7d910f9c2cd8c9d898098
Vladimir <andiwillfly@gmail.com>
2020-12-02 13:11:46 +0200

    dd

--- setupBot.js
+++ setupBot.js
@@ -40,3 +40,15 @@ async function start() {
 }
 
 start();
+
+const express = require('express');
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
