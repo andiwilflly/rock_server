516a8d471b2ea9c32f8965c4c9fc5ec2adf99c6f
Vladimir <andiwillfly@gmail.com>
2020-12-09 15:48:20 +0200

    d

--- TEST.js
+++ TEST.js
@@ -1,8 +1,27 @@
-const fs = require('fs');
-const { getChart } = require('billboard-top-100');
+const fetch = require('request');
 
-getChart((err, chart) => {
-    if (err) console.log(err);
-    console.log(chart);
-    fs.writeFileSync('./CHART.json', JSON.stringify(chart));
-});
+
+// https://developer.foursquare.com/docs/places-api/endpoints/
+fetch(
+    {
+        url: 'https://api.foursquare.com/v2/venues/search',
+        method: 'GET',
+        qs: {
+            client_id: 'IPJVISTE3A50N5EGPTPGVJHMJD3DCSKWWID3HTUAOEHFN2ZY',
+            client_secret: 'B3TP4415NUZSBLQWEJI0MFMG405BDJOBVXP1WTHS54Q0WUKC',
+            // ll: '40.7243,-74.0018',
+            near: 'Киев',
+            categoryId: '4deefb944765f83613cdba6e',
+            // query: 'parks',
+            v: '20180323',
+            limit: 1,
+        },
+    },
+    function(err, res, body) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(body);
+        }
+    }
+);
