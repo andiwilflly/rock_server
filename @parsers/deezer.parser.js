5928db5313eaef4757a27ee0747282c9b92a7f0a
Roman Lykhatskyi <romenlee@gmail.com>
2022-07-08 11:22:38 +0300

    deezer test fix

--- @parsers/deezer.parser.js
+++ @parsers/deezer.parser.js
@@ -70,7 +70,7 @@ async function parsePage(browser, group, album) {
             let img = '';
             try {
                 img = await page.evaluate(() => {
-                    return document.querySelector('img.sk__sc-10y8cfp-0').getAttribute('src');
+                    return document.querySelector('img.css-1phd9a0').getAttribute('src');
                 });
             } catch {}
 
@@ -126,9 +126,9 @@ async function start(browser, group, album) {
     console.log('✨ DEZZER PARSER:START...');
 
     // Cache
-    const prevResult = await global.MONGO_COLLECTION_PARSER.findOne({ _id: `deezer | ${group} | ${album}` });
+    /*const prevResult = await global.MONGO_COLLECTION_PARSER.findOne({ _id: `deezer | ${group} | ${album}` });
     if(prevResult) console.log('🌼 MONGO DB | DEEZER PARSER: return prev result...');
-    if(prevResult && !prevResult.link.includes('search?')) return prevResult;
+    if(prevResult && !prevResult.link.includes('search?')) return prevResult;*/
 
     const response = await parsePage(browser, group, album);
 
